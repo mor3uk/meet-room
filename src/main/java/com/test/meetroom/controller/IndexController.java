ce0e5545ec30f1278cfc18397b89b335b017b51a
Danila Shurygin <oldegs2017@gmail.com>
2021-08-02 18:39:17 +0400

    setup home page

--- src/main/java/com/test/meetroom/controller/IndexController.java
+++ src/main/java/com/test/meetroom/controller/IndexController.java
@@ -1,13 +1,18 @@
 package com.test.meetroom.controller;
 
+import com.test.meetroom.entity.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String meetRoom() {
+    public String meetRoom(Authentication authentication, Model model) {
+        User user = (User) authentication.getPrincipal();
+        model.addAttribute("username", user.getUsername());
         return "index";
     }
 }
